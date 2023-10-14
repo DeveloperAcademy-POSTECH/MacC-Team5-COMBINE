ef99ece71af79a9e2c77260570570a68686d262d
JOY <jypjoy@gmail.com>
2023-10-14 20:32:16 +0900

    [Setting] 프로젝트 생성

---
+++ Codingdong-iOS/Codingdong-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Codingdong-iOS
+//
+//  Created by Joy on 10/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
