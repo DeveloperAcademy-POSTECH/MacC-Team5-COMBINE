370310654d53eb37ab76724a24225381d63c2494
BAE <lexon1209@gmail.com>
2023-10-18 14:08:17 +0900

    [Fix] FontManager 수정사항 반영
    - pretendard 함수 return에 nil-coalescing 적용
    - extension으로 사용성 개선

--- Codingdong-iOS/Codingdong-iOS/Util/Manager/FontManager.swift
+++ Codingdong-iOS/Codingdong-iOS/Util/Manager/FontManager.swift
@@ -49,11 +49,50 @@ struct FontManager {
     /// type - pretendard type
     /// size - AX1 font size.
     /// HeadLine 사용시 body 선택 후 semibold 적용시켜야함.
-    func pretendard(_ type: PretendardType, _ size: FontSize) -> UIFont! {
-        return UIFont(name: type.rawValue, size: size.rawValue)
+    func pretendard(_ type: PretendardType, _ size: FontSize) -> UIFont {
+        return UIFont(name: type.rawValue, size: size.rawValue) ?? UIFont.systemFont(ofSize: size.rawValue)
     }
     
 }
 
+extension FontManager {
+    
+    static func p_Black(_ size: FontSize) -> UIFont {
+        FontManager.shared.pretendard(.black, size)
+    }
+    
+    static func p_Bold(_ size: FontSize) -> UIFont {
+        FontManager.shared.pretendard(.bold, size)
+    }
+    
+    static func p_ExBold(_ size: FontSize) -> UIFont {
+        FontManager.shared.pretendard(.extrabold, size)
+    }
+    
+    static func p_ExLight(_ size: FontSize) -> UIFont {
+        FontManager.shared.pretendard(.extralight, size)
+    }
+    
+    static func p_Light(_ size: FontSize) -> UIFont {
+        FontManager.shared.pretendard(.light, size)
+    }
+    
+    static func p_Medium(_ size: FontSize) -> UIFont {
+        FontManager.shared.pretendard(.medium, size)
+    }
+    
+    static func p_Regular(_ size: FontSize) -> UIFont {
+        FontManager.shared.pretendard(.regular, size)
+    }
+    
+    static func p_semiBold(_ size: FontSize) -> UIFont {
+        FontManager.shared.pretendard(.semibold, size)
+    }
+    
+    static func p_Thin(_ size: FontSize) -> UIFont {
+        FontManager.shared.pretendard(.thin, size)
+    }
+    
+}
 
 
