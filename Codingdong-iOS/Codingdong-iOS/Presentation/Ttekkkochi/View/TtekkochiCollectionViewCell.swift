3e0c33e8c8a7e5cc7d27a8612379f1ec13a025e9
JOY <jypjoy@gmail.com>
2023-10-21 11:39:01 +0900

    [Feat] 떡 1개 View 구성

---
+++ Codingdong-iOS/Codingdong-iOS/Presentation/Ttekkkochi/View/TtekkochiCollectionViewCell.swift
@@ -0,0 +1,58 @@
+//
+//  TtekkochiCollectionViewCell.swift
+//  Codingdong-iOS
+//
+//  Created by Joy on 10/21/23.
+//
+
+import UIKit
+
+enum CellState {
+    case selected
+    case unselected
+}
+
+final class TtekkochiCollectionViewCell: UICollectionViewCell {
+
+    static let identifier = "TtekkochiCollectionViewCell"
+    
+    var block: Codingblock {
+        didSet {
+            nameLabel.text = block.value
+        }
+    }
+    
+    private let nameLabel: BasePaddingLabel = {
+        let label = BasePaddingLabel(padding: UIEdgeInsets(top: 15.0, left: 36.0, bottom: 15.0, right: 36.0))
+        label.font = FontManager.p_semiBold(.body)
+        label.textAlignment = .center
+        label.textColor = .gs70
+        label.backgroundColor = .secondary1
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 32
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        self.block = Codingblock(value: "", bgColor: .gs70)
+        super.init(frame: frame)
+        addSubview(nameLabel)
+        nameLabel.snp.makeConstraints {
+            $0.top.left.right.bottom.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configUI(_ type: CellState) {
+        switch type {
+        case .selected:
+            nameLabel.backgroundColor = block.bgColor
+            nameLabel.text = block.value
+        case .unselected:
+            nameLabel.backgroundColor = .gs70
+        }
+    }
+}
