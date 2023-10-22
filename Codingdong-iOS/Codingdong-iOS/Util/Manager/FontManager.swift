a18cf0cc6577780e531f890c62b92ba36ba63dd2
BAE <lexon1209@gmail.com>
2023-10-23 03:10:19 +0900

    [Refactor] Font Design Guide 반영
    - text button, navigation title 사이즈 추가
    - 각 style에 따라 새롭게 메서드 정리
    - 추후 FontManager 사용시 parameter 없이 사용 가능

--- Codingdong-iOS/Codingdong-iOS/Util/Manager/FontManager.swift
+++ Codingdong-iOS/Codingdong-iOS/Util/Manager/FontManager.swift
@@ -14,11 +14,11 @@ enum FontSize: CGFloat {
     case title1 = 38
     case title2 = 34
     case title3 = 31
-    // enum rawValue 중첩 불가 이슈로 주석 처리
-    //    case headline = 28
     case body = 28
+    case textbutton = 27
     case callout = 26
     case subhead = 25
+    case navigationtitle = 24
     case footnote = 23
     case caption1 = 22
     case caption2 = 20
@@ -47,7 +47,6 @@ struct FontManager {
     
     /// type - pretendard type
     /// size - AX1 font size.
-    /// HeadLine 사용시 body 선택 후 semibold 적용시켜야함.
     func pretendard(_ type: PretendardType, _ size: FontSize) -> UIFont {
         return UIFont(name: type.rawValue, size: size.rawValue) ?? UIFont.systemFont(ofSize: size.rawValue)
     }
@@ -90,6 +89,54 @@ extension FontManager {
     
     static func p_Thin(_ size: FontSize) -> UIFont {
         FontManager.shared.pretendard(.thin, size)
+    }
+    
+    static func largeTitle() -> UIFont {
+        FontManager.shared.pretendard(.regular, .largetitle)
+    }
+    
+    static func title1() -> UIFont {
+        FontManager.shared.pretendard(.regular, .title1)
+    }
+    
+    static func title2() -> UIFont {
+        FontManager.shared.pretendard(.regular, .title2)
+    }
+    
+    static func title3() -> UIFont {
+        FontManager.shared.pretendard(.regular, .title3)
+    }
+    
+    static func body() -> UIFont {
+        FontManager.shared.pretendard(.regular, .body)
+    }
+    
+    static func textbutton() -> UIFont {
+        FontManager.shared.pretendard(.semibold, .textbutton)
+    }
+    
+    static func callout() -> UIFont {
+        FontManager.shared.pretendard(.regular, .callout)
+    }
+    
+    static func subhead() -> UIFont {
+        FontManager.shared.pretendard(.semibold, .subhead)
+    }
+    
+    static func navigationtitle() -> UIFont {
+        FontManager.shared.pretendard(.semibold, .navigationtitle)
+    }
+    
+    static func footnote() -> UIFont {
+        FontManager.shared.pretendard(.regular, .footnote)
+    }
+    
+    static func caption1() -> UIFont {
+        FontManager.shared.pretendard(.regular, .caption1)
+    }
+    
+    static func caption2() -> UIFont {
+        FontManager.shared.pretendard(.regular, .caption2)
     }
     
 }
