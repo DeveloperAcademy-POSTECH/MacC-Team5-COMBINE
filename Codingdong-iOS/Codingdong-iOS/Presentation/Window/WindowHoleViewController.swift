f0afc3ff662aea2391477a1ed0af272249b0be6d
dtd1232 <dtd12321@gmail.com>
2023-11-08 17:34:49 +0900

    [Feat] 창문 스토리 시작화면 및 이미지 뷰 추가

---
+++ Codingdong-iOS/Codingdong-iOS/Presentation/Window/WindowHoleViewController.swift
@@ -0,0 +1,110 @@
+//
+//  WindowHoleViewController.swift
+//  Codingdong-iOS
+//
+//  Created by 이승용 on 11/6/23.
+//
+
+import UIKit
+import SnapKit
+
+final class WindowHoleViewController: UIViewController {
+    // MARK: - Components
+    private let naviLine: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white.withAlphaComponent(0.15)
+        return view
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "구멍을 탭해서 문 밖에 누가 있는지 확인해주세요."
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = FontManager.body()
+        label.textColor = .gs10
+        label.numberOfLines = 0
+        label.lineBreakMode = .byCharWrapping
+        return label
+    }()
+    
+    private let windowImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "initialDoor")
+        return imageView
+    }()
+    
+    private let tigerHandHoleImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "tigerHandsHoleBefore.png")
+        return imageView
+    }()
+    
+    private let nextButton = CommonButton()
+    private lazy var nextButtonViewModel = CommonbuttonModel(title: "다음",font: FontManager.textbutton(), titleColor: .primary1, backgroundColor: .primary2) { [weak self] in
+        self?.navigationController?.pushViewController(GiveTtekkViewController(), animated: false)
+    }
+    
+    // MARK: - View init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gs90
+        setupNavigationBar()
+        addComponents()
+        setConstraints()
+        nextButton.setup(model: nextButtonViewModel)
+    }
+    
+    func setupNavigationBar() {
+        view.addSubview(naviLine)
+        self.title = "남매의 집에 도착한 호랑이"
+        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.gs20, .font: FontManager.navigationtitle()]
+        self.navigationController?.navigationBar.tintColor = .gs20
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "books.vertical"),
+            style: .plain,
+            target: self,
+            action: .none
+        )
+    }
+    
+    func addComponents() {
+        [titleLabel, windowImageView, tigerHandHoleImageView, nextButton].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    func setConstraints() {
+        naviLine.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(106)
+            $0.left.right.equalToSuperview()
+            $0.height.equalTo(0.33)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(122)
+            $0.left.equalToSuperview().offset(16)
+            $0.right.equalToSuperview().offset(-16)
+        }
+        
+        windowImageView.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(51)
+            $0.right.equalToSuperview().offset(-51)
+            $0.top.equalToSuperview().offset(214)
+            $0.bottom.equalToSuperview().offset(-160)
+        }
+        
+        tigerHandHoleImageView.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(102)
+            $0.top.equalToSuperview().offset(401)
+        }
+    }
+    
+    func animatedImages(for name: String) -> [UIImage] {
+        var images = [UIImage]()
+        let image1 = UIImage(named: "tigerHandHoleBefore.png")!
+        let image2 = UIImage(named: "tigerHandHoleAfter.png")!
+        images.append(image1)
+        images.append(image2)
+        return images
+    }
+}
---
+++ Codingdong-iOS/Codingdong-iOS/Presentation/Window/WindowStartViewController.swift
@@ -0,0 +1,84 @@
+//
+//  WindowStartViewController.swift
+//  Codingdong-iOS
+//
+//  Created by 이승용 on 11/3/23.
+//
+
+import UIKit
+import SnapKit
+
+final class WindowStartViewController: UIViewController {
+    
+    // MARK: - Components
+    private let naviLine: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white.withAlphaComponent(0.15)
+        return view
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "아직 배가 고픈 호랑이는 오누이도 잡아먹고 싶어졌어요. \n\n그래서 꾀를 내어 엄마로 변장해 오누이의 집으로 찾아갔어요. \n\n오누이를 도와 문 밖의 무서운 호랑이의 정체를 밝혀볼까요?"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = FontManager.body()
+        label.textColor = .gs10
+        label.numberOfLines = 0
+        label.lineBreakMode = .byCharWrapping
+        return label
+    }()
+    
+    private let nextButton = CommonButton()
+    private lazy var nextButtonViewModel = CommonbuttonModel(title: "구멍엿보기",font: FontManager.textbutton(), titleColor: .primary1, backgroundColor: .primary2) { [weak self] in
+        self?.navigationController?.pushViewController(WindowHoleViewController(), animated: false)
+    }
+    
+    // MARK: - View init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gs90
+        setupNavigationBar()
+        addComponents()
+        setConstraints()
+        nextButton.setup(model: nextButtonViewModel)
+    }
+    
+    func setupNavigationBar() {
+        view.addSubview(naviLine)
+        self.title = "남매의 집에 도착한 호랑이"
+        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.gs20, .font: FontManager.navigationtitle()]
+        self.navigationController?.navigationBar.tintColor = .gs20
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "books.vertical"),
+            style: .plain,
+            target: self,
+            action: .none
+        )
+    }
+    
+    func addComponents() {
+        view.addSubview(titleLabel)
+        view.addSubview(nextButton)
+    }
+    
+    func setConstraints() {
+        naviLine.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(106)
+            $0.left.right.equalToSuperview()
+            $0.height.equalTo(0.33)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(122)
+            $0.left.equalToSuperview().offset(16)
+            $0.right.equalToSuperview().offset(-16)
+        }
+        
+        nextButton.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(Constants.Button.buttonPadding)
+            $0.right.equalToSuperview().offset(-Constants.Button.buttonPadding)
+            $0.bottom.equalToSuperview().offset(-Constants.Button.buttonPadding * 2)
+            $0.height.equalTo(72)
+        }
+    }
+}
