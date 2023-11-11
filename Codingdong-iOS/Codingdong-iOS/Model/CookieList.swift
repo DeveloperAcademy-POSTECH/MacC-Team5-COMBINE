90de52baecf47d91d3262e2ad107e707ece0b94e
JOY <jypjoy@gmail.com>
2023-11-11 14:09:53 +0900

    [Feat] 개념 간식 모델 선언
    - 내 책ㄹ장 UI 구성을 위한 모델 선언

---
+++ Codingdong-iOS/Codingdong-iOS/Model/CookieList.swift
@@ -0,0 +1,18 @@
+//
+//  CookieList.swift
+//  Codingdong-iOS
+//
+//  Created by Joy on 11/11/23.
+//
+
+import Foundation
+
+struct CookieList {
+    var haveCookie: Bool
+    var cookie: [Cookie]
+}
+
+struct Cookie {
+    var image: String
+    var concept: String
+}
