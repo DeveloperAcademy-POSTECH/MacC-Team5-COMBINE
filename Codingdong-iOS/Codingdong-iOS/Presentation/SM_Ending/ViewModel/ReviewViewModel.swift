229bf08145b6f10d01b8f4af8006aca2939cfda5
JOY <jypjoy@gmail.com>
2023-11-12 21:47:41 +0900

    [Feat] 코딩 개념 복습  뷰모델 추가

---
+++ Codingdong-iOS/Codingdong-iOS/Presentation/SM_Ending/ViewModel/ReviewViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  ReviewViewModel.swift
+//  Codingdong-iOS
+//
+//  Created by Joy on 11/12/23.
+//
+
+import UIKit
+import Combine
+
+protocol ReviewViewModelRepresentable {
+    func endStory()
+}
+
+final class ReviewViewModel: ReviewViewModelRepresentable {
+    private var cancellables = Set<AnyCancellable>()
+    var route: AnyPublisher<UIViewController, Never> { self.sendRoute.eraseToAnyPublisher() }
+    var sendRoute: PassthroughSubject<UIViewController, Never> = .init()
+    
+    func endStory() {
+        sendRoute.send(MyBookShelfViewController())
+    }
+}
