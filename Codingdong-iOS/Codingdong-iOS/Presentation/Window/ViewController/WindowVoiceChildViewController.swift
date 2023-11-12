bb3ec22aaba05ce672655d458cb7c7156d5df85d
dtd1232 <dtd12321@gmail.com>
2023-11-12 17:51:12 +0900

    [Feat] Timer 구현

--- Codingdong-iOS/Codingdong-iOS/Presentation/Window/ViewController/WindowVoiceChildViewController.swift
+++ Codingdong-iOS/Codingdong-iOS/Presentation/Window/ViewController/WindowVoiceChildViewController.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 import SnapKit
+import Log
 
 final class WindowVoiceChildViewController: UIViewController {
+    
+    var mTimer: Timer?
+    var initialCountNumber: Int = 3
     
     private let containerView: UIView = {
        let view = UIView()
@@ -16,9 +20,9 @@ final class WindowVoiceChildViewController: UIViewController {
         return view
     }()
     
-    private let titleLabel: UILabel = {
+    private lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "3"
+        label.text = String(initialCountNumber)
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = FontManager.p_Bold(FontSize(rawValue: 64) ?? .largetitle)
         label.textColor = .white
@@ -39,17 +43,37 @@ final class WindowVoiceChildViewController: UIViewController {
         titleLabel.snp.makeConstraints {
             $0.centerX.centerY.equalToSuperview()
         }
-        
-        
+        Log.t("View Did Load")
+        onTimerStart()
     }
-//    func addComponents() {
-//        containerView.addSubview(titleLabel)
-//    }
     
-//    func setConstraints() {
-//        titleLabel.snp.makeConstraints {
-//            $0.left.right.equalToSuperview()
-//            $0.top.bottom.equalToSuperview()
-//        }
-//    }
+    func onTimerStart() {
+        if let timer = mTimer {
+            if !timer.isValid {
+                mTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCallBack), userInfo: nil, repeats: true)
+            }
+        } else {
+            mTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCallBack), userInfo: nil, repeats: true)
+        }
+    }
+    
+    func onTimerEnd() {
+        if let timer = mTimer {
+            if initialCountNumber == 0 {
+                timer.invalidate()
+                // TODO: STT 실행
+                Log.t("Timer invalidate")
+                Log.t("\(initialCountNumber)")
+            }
+        }
+    }
+    
+    @objc func timerCallBack() {
+        initialCountNumber -= 1
+        if initialCountNumber == 0 {
+            onTimerEnd()
+        }
+        Log.t("\(initialCountNumber)")
+        titleLabel.text = String(initialCountNumber)
+    }
 }
