1f38b0c0f6fd71bb37357d50ed38c6a09a0dbd08
BAE <lexon1209@gmail.com>
2023-11-14 01:11:34 +0900

    [Refactor] 떡 주기 뷰 코드 리팩터
    - 수정 중

--- Codingdong-iOS/Codingdong-iOS/Presentation/SM_Concept2/ViewController/GiveTtekkViewController.swift
+++ Codingdong-iOS/Codingdong-iOS/Presentation/SM_Concept2/ViewController/GiveTtekkViewController.swift
@@ -11,7 +11,9 @@ import CoreMotion
 
 final class GiveTtekkViewController: UIViewController {
     
-    private var hapticManager: HapticManager?
+    private let padding = Constants.View.padding
+    
+    private let hapticManager = HapticManager()
     
     private var maxShakeCount = 5
     
@@ -29,6 +31,14 @@ final class GiveTtekkViewController: UIViewController {
         let view = UIView()
         view.frame = .zero
         return view
+    }()
+    
+    private let ttekkStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 6
+        stack.distribution = .fill
+        return stack
     }()
     
     private let naviLine: UIView = {
@@ -69,7 +79,7 @@ final class GiveTtekkViewController: UIViewController {
             $0.left.right.equalToSuperview()
             $0.height.equalTo(0.33)
         }
-
+        
         self.title = "호랑이를 마주친 엄마"
         self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.gs20, .font: FontManager.navigationtitle()]
         self.navigationController?.navigationBar.tintColor = .gs20
@@ -82,39 +92,99 @@ final class GiveTtekkViewController: UIViewController {
     }
     
     func addComponents() {
-        [storyLabel, rectanglesContainerView].forEach { view.addSubview($0) }
-        ttekkStackView()
+        [storyLabel, rectanglesContainerView, ttekkStackView].forEach { view.addSubview($0) }
+        /*ttekkStackView*/()
+        
+        let ttekks = (1...5).map { _ in
+            return self.createTtekkViews(height: 112, cornerRadius: 56)
+        }
+        ttekks.forEach { ttekkStackView.addArrangedSubview($0) }
     }
     
     func setConstraints() {
         storyLabel.snp.makeConstraints {
-            $0.height.equalTo(68)
-            $0.width.equalTo(358)
-            $0.leading.equalToSuperview().offset(16)
-            $0.top.equalToSuperview().offset(122)
+            $0.top.equalTo(naviLine.snp.bottom).offset(padding)
+            $0.left.right.equalToSuperview().inset(padding)
         }
         
         rectanglesContainerView.snp.makeConstraints {
             $0.width.height.equalToSuperview()
         }
-    }
-    
-    private func handleShake() {
-        maxShakeCount -= 1
         
-        if !ttekkRectangleArray.isEmpty {
-            let removeTukk = ttekkRectangleArray.removeLast()
-            removeTukk.removeFromSuperview()
-            self.hapticManager = HapticManager()
-            self.hapticManager?.playNomNom()
-            SoundManager.shared.playTTS("\(maxShakeCount)개")
-        } else {
-            self.navigationController?.pushViewController(TigerAnimationViewController(), animated: false)
+        ttekkStackView.snp.makeConstraints {
+            $0.top.equalTo(storyLabel.snp.bottom).offset(72)
+            $0.left.right.bottom.equalToSuperview().inset(4)
         }
     }
     
-    private func countShake() {
+    //    private func makeTtekks() {
+    //        let ttekks = (1...5).map { <#Int#> in
+    //            <#code#>
+    //        }
+    //
+    //        for i in 0..<maxShakeCount {
+    //            let rect = UIView()
+    //            rect.backgroundColor = UIColor.white
+    //            rect.layer.cornerRadius = 60
+    //            rectanglesContainerView.addSubview(rect)
+    //            ttekkRectangleArray.append(rect)
+    //
+    //            rect.snp.makeConstraints {
+    //                $0.leading.equalToSuperview().offset(4)
+    //                $0.trailing.equalToSuperview().offset(-4)
+    //                $0.bottom.equalToSuperview().offset(-4 - i*118)
+    //                $0.top.equalToSuperview().offset(728 - i*118)
+    //            }
+    //        }
+    //    }
+    
+}
+
+extension GiveTtekkViewController {
+    
+    @objc
+    func popThisView() {
+        self.navigationController?.popToRootViewController(animated: false)
+    }
+    
+    func createTtekkViews(height: CGFloat, cornerRadius: CGFloat) -> UIView {
+        let ttekkView = UIView()
+        ttekkView.backgroundColor = .white
+        ttekkView.layer.cornerRadius = cornerRadius
+        ttekkView.snp.makeConstraints {
+            $0.height.equalTo(height)
+        }
+        return ttekkView
+    }
+    
+    func handleShake() {
+        //        maxShakeCount -= 1
+        //        if !ttekkRectangleArray.isEmpty {
+        //            let removeTukk = ttekkRectangleArray.removeLast()
+        //            removeTukk.removeFromSuperview()
+        //            self.hapticManager = HapticManager()
+        //            self.hapticManager?.playNomNom()
+        //            SoundManager.shared.playTTS("\(maxShakeCount)개")
+        //        } else {
+        //            self.navigationController?.pushViewController(TigerAnimationViewController(), animated: false)
+        //        }
         
+        let ttekks = ttekkStackView.arrangedSubviews
+        
+        if ttekks.count != 0 {
+            if let poppedView = ttekks.last {
+                ttekkStackView.removeArrangedSubview(poppedView)
+                ttekkStackView.removeFromSuperview()
+                self.hapticManager?.playNomNom()
+                SoundManager.shared.playTTS("\(ttekks.count)개")
+            }
+        } else {
+            self.navigationController?.pushViewController(TigerAnimationViewController(), animated: false)
+        }
+        
+    }
+    
+    func countShake() {
         // CoreMotion을 사용한 방법
         // Threshold를 조절해서 인식 강도 조절 가능
         if motionManager.isAccelerometerAvailable {
@@ -129,28 +199,6 @@ final class GiveTtekkViewController: UIViewController {
                 }
             }
         }
-    }
-    
-    private func ttekkStackView() {
-        for i in 0..<maxShakeCount {
-            let rect = UIView()
-            rect.backgroundColor = UIColor.white
-            rect.layer.cornerRadius = 60
-            rectanglesContainerView.addSubview(rect)
-            ttekkRectangleArray.append(rect)
-            
-            rect.snp.makeConstraints {
-                $0.leading.equalToSuperview().offset(4)
-                $0.trailing.equalToSuperview().offset(-4)
-                $0.bottom.equalToSuperview().offset(-4 - i*118)
-                $0.top.equalToSuperview().offset(728 - i*118)
-            }
-        }
-    }
-    
-    @objc
-    private func popThisView() {
-        self.navigationController?.popToRootViewController(animated: false)
     }
     
 }
