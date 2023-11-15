4f95157bab356a73ebd3a48770134883f148b6ea
JOY <jypjoy@gmail.com>
2023-11-15 11:29:10 +0900

    [Design] 문구 수정

--- Codingdong-iOS/Codingdong-iOS/Presentation/SM_Concept2/ViewController/WindowStartViewController.swift
+++ Codingdong-iOS/Codingdong-iOS/Presentation/SM_Concept2/ViewController/WindowStartViewController.swift
@@ -39,11 +39,11 @@ final class WindowStartViewController: UIViewController, ConfigUI {
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.text = """
-        아직 배가 고픈 호랑이는 오누이도 잡아먹고 싶어졌어요.
+        아직 배가 고픈 호랑이는 오누이도 잡아 먹고 싶어졌어요.
         
         그래서 꾀를 내어 엄마로 변장해 오누이의 집으로 찾아 갔어요.
         
-        오누이를 도와 문 밖의 무서운 호랑이의 정체를 밝혀볼까요?
+        오누이를 도와 문 밖의 무서운 호랑이의 정체를 밝혀 볼까요?
         """
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = FontManager.body()
