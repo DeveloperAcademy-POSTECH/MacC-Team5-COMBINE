cce3a2f15ae219bf66a7e60747a497c47fd69e88
JOY <jypjoy@gmail.com>
2023-11-27 03:33:01 +0900

    [Refactor] 공용 DB 추상화

--- Codingdong-iOS/Codingdong-iOS/Data/CodingdongDBService.swift
+++ Codingdong-iOS/Codingdong-iOS/Data/CodingdongDBService.swift
@@ -11,51 +11,36 @@ import Log
 
 final class CodingdongDBService {
     static var shared = CodingdongDBService()
-    var container: ModelContainer?
-    var context: ModelContext?
-    
-    init() {
+
+    var context: ModelContext
+    var container: ModelContainer = {
+        let schema = Schema([FableData.self, FoodList.self, Food.self])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
         do {
-            container = try ModelContainer(for: FableData.self, FoodList.self)
-            if let container { context = ModelContext(container) }
-            self.fetchFable { data, error in
-                if let error { Log.e(error) }
-                if data?.count == 0 { fables.forEach { self.context?.insert($0) } }
-            }
-        } catch { Log.e(error.localizedDescription) }
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+    
+    init() { context = ModelContext(container) }
+    
+    /// CREATE
+    func createItem<T: PersistentModel> (_ item: T) {
+        context.insert(item)
     }
     
-    // 전래동화
-    func fetchFable(onCompletion: @escaping([FableData]?, Error?) -> Void) {
-        let descriptor = FetchDescriptor<FableData>(sortBy: [SortDescriptor<FableData>(\.title, order: .reverse)])
-        
-        if let context {
-            do {
-                let data = try context.fetch(descriptor)
-                onCompletion(data,nil)
-            } catch { onCompletion(nil,error) }
+    /// READ
+    func readItems<T: PersistentModel>(key: [SortDescriptor<T>]?, onCompletion: @escaping([T]?, Error?) -> ()) {
+        guard let key = key else { return }
+        let descriptor = FetchDescriptor<T>(sortBy: key)
+        do {
+            let data = try context.fetch(descriptor)
+            onCompletion(data,nil)
+        } catch {
+            onCompletion(nil,error)
         }
     }
-    
-    func updateFable(fable: FableData, checkRead: Bool) {
-        let readFable = fable
-        readFable.isRead = checkRead
-    }
-    
-    // 개념 간식
-    func fetchFoodList(onCompletion: @escaping([FoodList]?, Error?) -> Void) {
-        let descriptor = FetchDescriptor<FoodList>()
-        
-        if let context {
-            do {
-                let data = try context.fetch(descriptor)
-                onCompletion(data,nil)
-            } catch { onCompletion(nil,error) }
-        }
-    }
-    
-    func saveFoodList(foodList: FoodList) {
-        guard let context = context else { return }
-        context.insert(foodList)
-    }
+    /// DELETE
+    func deleteItems<T:PersistentModel>(_ item: T) { context.delete(item) }
 }
