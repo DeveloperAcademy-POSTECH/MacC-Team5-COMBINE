0148a369f74d8f2bbc30d1a1d7a3b78a2032fc92
JOY <jypjoy@gmail.com>
2023-11-26 15:13:51 +0900

    [Feat] 개념 간식 모음 모델 선언

--- Codingdong-iOS/Codingdong-iOS/Model/Food.swift
+++ Codingdong-iOS/Codingdong-iOS/Model/Food.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class FoodList {
+    var haveFood: Bool
+    var food: [Food]
+    
+    init(haveFood: Bool, food: [Food]) {
+        self.haveFood = haveFood
+        self.food = food
+    }
+}
+
+@Model
+class Food {
+    var image: String
+    var concept: String
+    
+    init(image: String, concept: String) {
+        self.image = image
+        self.concept = concept
+    }
+}
+
+
+
+
